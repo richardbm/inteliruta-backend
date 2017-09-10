3bb46bb0f346a6cd0c560fb46fe2fb38e60f97d0
Richard <rhrichardbm@gmail.com>
2017-09-10 11:57:07 -0400

    docs updated

---
+++ rides/tests/test_integration/test_response_offer.py
@@ -0,0 +1,26 @@
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from model_mommy import mommy
+from collections import OrderedDict
+
+
+class TestResponseOffer(APITestCase):
+
+    def setUp(self):
+        self.c = APIClient()
+
+    def test_response_offer(self):
+        user = mommy.make("accounts.user")
+        offer = mommy.make("rides.offer", owner=user,
+                             _fill_optional=True)
+
+        self.c.force_authenticate(user)
+
+        data = {
+            "text": "Puedes llevarme?"
+        }
+
+        response = self.c.post("/offers/{}/request/".format(offer.id), data)
+        self.assertEqual(response.status_code, 201)
+        self.assertDictEqual.__self__.maxDiff = None
+
